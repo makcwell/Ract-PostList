f8f82efa9e624cd569ba9c7a47a234668a7f35b9
Maxim Alexeev <C:\Users\\AppData\Roaming\The Bat!>
2023-02-10 20:55:49 +0300

    layout: add mainlist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import s from './app.css';
 import Header from './Components/Header/header'
+import MainList from "./Components/MainList/mainList";
 function App() {
     return (
         <>
             <Header/>
+            <MainList/>
 
         </>
     );
---
+++ src/Components/MainList/mainList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './mainList.module.css'
+function MainList(props) {
+    return (
+        <div className={s.mainList}>
+            <div className={s.container}>
+
+            </div>
+        </div>
+    );
+}
+
+export default MainList;
+
