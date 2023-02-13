fc17f5d2abcbf752a621e108cd3262edb2a8338b
Aleksei Zhuikov <design.azv@gmail.com>
2023-02-13 23:58:43 +0300

    added card

--- src/Components/MainList/PostList/postList.jsx
+++ src/Components/MainList/PostList/postList.jsx
@@ -1,8 +1,11 @@
+import { Grid } from '@mui/material';
 import React from 'react';
 
 function PostList(props) {
     return (
-        <div></div>
+        <Grid container spacing={{ xs: 2, sm: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {props.children}
+        </Grid>
     );
 }
 
--- src/Components/MainList/mainList.jsx
+++ src/Components/MainList/mainList.jsx
@@ -1,12 +1,14 @@
+// import { Grid } from '@mui/material';
 import React from 'react';
 import s from './mainList.module.css'
 function MainList(props) {
     return (
         <div className={s.mainList}>
-            <div className={s.container}>
-
-            </div>
+            {props.children}
         </div>
+
+
+
     );
 }
 
