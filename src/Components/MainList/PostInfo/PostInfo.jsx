9c66e328071fe5d8d1abdd5311327c772e199f94
Maxim Alexeev <C:\Users\\AppData\Roaming\The Bat!>
2023-03-26 00:31:44 +0300

    Postinfo

--- src/Components/MainList/PostInfo/PostInfo.jsx
+++ src/Components/MainList/PostInfo/PostInfo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
+import {Box, Container, CardMedia, Typography, Avatar} from "@mui/material";
 
 function PostInfo({cards}) {
     const params = useParams()
@@ -9,10 +10,43 @@ function PostInfo({cards}) {
     console.log('POSTINFO>>>',post)
     return (
         <>
-            <div> {post._id}</div>
-            <div> {post.author.name}</div>
-            <div> PostInfo component</div>
-            <div> PostInfo component</div>
+            <Container  maxWidth="lg" sx={{
+                display: "flex",
+                minheight: '600px',
+                backgroundColor: 'white',
+                borderRadius: '20px',
+                padding: '24px',
+            }}>
+                <Box sx={{maxWidth: '576px'}}>
+
+                    <CardMedia
+                        component="img"
+                        alt="green iguana"
+                        // height="250"
+                        image={post.image}
+                        sx={{borderRadius: '15px'}}
+                    />
+                    <Typography variant={'h4'}>{post.title}</Typography>
+                    <Typography>{post.text}</Typography>
+
+                </Box>
+                <Box sx={{maxWidth: '576px'}}>
+
+                    <Box sx={{display: "flex", alignItems: 'center', justifyContent: 'space-around', width: '576px'}}>
+                        <Box>
+                            <Typography>Автор: {post.author.name}</Typography>
+                            <br/>
+                            <Typography>Прфессия: {post.author.about}</Typography>
+                        </Box>
+                        <Avatar src={post.author.avatar} sx={{width: 56, height: 56}} alt="Remy Sharp"/>
+
+
+                    </Box>
+
+                </Box>
+
+
+            </Container>
         </>
     );
 }
