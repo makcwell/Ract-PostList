136a3c55038e98e9e6189b3db374a69e6ad55296
Aleksei Zhuikov <design.azv@gmail.com>
2023-03-29 14:28:06 +0300

    fixed: reviewed chancges: add icon like comment from mui /changed btn srnd comment / and other things

--- src/Components/MainList/PostList/CardInfo/CardInfo.jsx
+++ src/Components/MainList/PostList/CardInfo/CardInfo.jsx
@@ -1,17 +1,14 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { Avatar, Button, CardContent, CardHeader, CardMedia, Chip } from '@mui/material';
+import { Box, Paper, Avatar, Button, CardContent, CardHeader, CardMedia, Chip, Grid } from '@mui/material';
 import s from '../CardInfo/card-info.module.css'
 import { Stack } from '@mui/system';
-import { useEffect } from 'react';
-import apiPosts from '../../../../API/PostsApi';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import SendIcon from '@mui/icons-material/Send';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
 
 const Item = styled(Paper)(({ theme }) => ({
     // backgroundColor: theme.palette.mode === 'white' ? '#1A2027' : '#fff',
@@ -63,7 +60,7 @@ export function CardInfo({ cards }) {
         month: 'long',
         year: 'numeric',
     }
-    let whenCreatedPost = new Date(post.created_at).toLocaleString('ru', options).slice(0, -3);
+    let whenCreatedPost = new Date(post?.created_at).toLocaleString('ru', options).slice(0, -3);
 
     return (
         <Box sx={{ flexGrow: 1 }}
@@ -91,7 +88,7 @@ export function CardInfo({ cards }) {
                                 boxShadow: '0px 5px 10px 2px rgba(17, 18, 19, 0.5)',
 
                             }}
-                            image={post.image}
+                            image={post?.image}
                             title="фото"
                             alt="фото"
 
@@ -112,12 +109,12 @@ export function CardInfo({ cards }) {
                                         backgroundColor: 'teal',
                                         width: 56, height: 56,
                                     }}
-                                        src={post.author.avatar}
+                                        src={post?.author.avatar}
                                     />
 
                                 }
 
-                                title={post.author.name}
+                                title={post?.author.name}
                                 subheader={whenCreatedPost}
                             />
                         </div>
@@ -134,11 +131,9 @@ export function CardInfo({ cards }) {
 
                                     {/* Лайки карточки */}
                                     <div className={s.boxSvg}>
-                                        <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" >
-                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 21c.211 0 .514-.137.735-.265C18.405 17.205 22 13.098 22 8.922 22 5.45 19.553 3 16.29 3c-1.863 0-3.374.863-4.29 2.186C11.104 3.873 9.573 3 7.71 3 4.447 3 2 5.451 2 8.922c0 4.176 3.595 8.284 9.275 11.813.211.128.514.265.725.265Zm0-1.657c-.04 0-.11-.049-.201-.117C7.579 16.5 3.62 12.569 3.62 8.921c0-2.608 1.732-4.344 4.069-4.344 1.893 0 2.98 1.147 3.625 2.128.272.392.443.5.685.5.242 0 .393-.118.685-.5.695-.96 1.742-2.128 3.625-2.128 2.337 0 4.069 1.736 4.069 4.344 0 3.647-3.958 7.578-8.168 10.303-.1.07-.17.118-.211.118Z" fill="#7B8E98"></path>
-                                            <path clip-rule="evenodd" d="M12 19.343c-.04 0-.11-.049-.201-.117C7.579 16.5 3.62 12.569 3.62 8.921c0-2.608 1.732-4.344 4.069-4.344 1.893 0 2.98 1.148 3.625 2.128.272.392.443.5.685.5.242 0 .393-.118.685-.5.695-.96 1.742-2.128 3.625-2.128 2.337 0 4.069 1.736 4.069 4.344 0 3.647-3.958 7.578-8.168 10.303-.1.07-.17.118-.211.118Z"></path>
-                                        </svg>
-                                        {post.likes.length}
+                                        {/* <Like /> */}
+                                        <FavoriteBorderIcon />
+                                        {post?.likes.length}
                                     </div>
 
                                     {/* Хештеги карточки */}
@@ -148,7 +143,7 @@ export function CardInfo({ cards }) {
                                         // backgroundColor='tomato'
                                         spacing={1}>
 
-                                        {post.tags.map((tag) =>
+                                        {post?.tags.map((tag) =>
                                             <Chip sx={{ marginBottom: '5px' }} label={tag} key={tag} size="small" color="success" />
                                         )}
 
@@ -166,7 +161,7 @@ export function CardInfo({ cards }) {
                 <Grid item xs={12}>
                     <Item sx={{ fontSize: '1rem' }}>
                         <span><b>Название поста:</b></span>
-                        <span className={s.postDescription}>{post.title}</span>
+                        <span className={s.postDescription}>{post?.title}</span>
                     </Item>
                 </Grid>
 
@@ -174,14 +169,13 @@ export function CardInfo({ cards }) {
                 <Grid item xs={12}>
                     <Item sx={{ fontSize: '1rem' }}>
                         <span><b>Описание поста:</b> </span>
-                        <span className={s.postDescription}>{post.text}</span>
+                        <span className={s.postDescription}>{post?.text}</span>
                     </Item>
                 </Grid>
 
-                {/* Кнопка добавления комментариев */}
+                {/* Кнопка открытия / закрытия формы добавления комментария */}
                 <Grid item>
                     <Button variant="contained"
-                        // onClick={() => setShowFormComment(true)}
                         onClick={openFormComment}
                     >{showFormComment ? 'Скрыть комментарий' : 'Добавить комментарий'}
                     </Button>
@@ -206,7 +200,7 @@ export function CardInfo({ cards }) {
                                 />
                                 {errors?.comment && <span className={s.errorComment}>{errors.comment?.message}</span>}
 
-                                <Button sx={{ maxWidth: '200px', marginBottom: '2rem' }} size="large" type='submit' variant="contained" color="success">
+                                <Button sx={{ maxWidth: '200px', marginBottom: '2rem' }} size="large" type='submit' variant="contained" color="success" endIcon={<SendIcon />}>
                                     Добавить
                                 </Button>
                             </form>
@@ -218,9 +212,9 @@ export function CardInfo({ cards }) {
                 <Grid item xs={12}>
                     <Item sx={{ maxHeight: '450px', overflow: 'hidden', overflowY: 'scroll', border: '1px solid #ccc' }}>
 
-                        {post.comments.length !== 0 ?
+                        {post?.comments.length !== 0 ?
 
-                            (post.comments.map((comment, i) =>
+                            (post?.comments.map((comment, i) =>
 
                                 <div className={s.commentWrapper} key={i}>
                                     <div className={s.userInfoComment}>
@@ -237,12 +231,12 @@ export function CardInfo({ cards }) {
                                                     width: 32, height: 32,
 
                                                 }}
-                                                    src={post.author.avatar}
+                                                    src={post?.author.avatar}
                                                 />
 
                                             }
 
-                                            title={post.author.name}
+                                            title={post?.author.name}
                                             subheader={whenCreatedPost}
                                         />
                                     </div>
