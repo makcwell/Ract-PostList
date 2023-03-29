ce2eea23f038dba8554ed34d14ae74891579dd40
Maxim Alexeev <C:\Users\\AppData\Roaming\The Bat!>
2023-03-29 20:22:25 +0300

    my form

--- src/Components/MainList/PostInfo/PostInfo.jsx
+++ src/Components/MainList/PostInfo/PostInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
-import {Box, Container, CardMedia, Typography, Avatar} from "@mui/material";
+import {Box, Container, CardMedia, Typography, Avatar, Grid} from "@mui/material";
 
 function PostInfo({cards}) {
     const params = useParams()
@@ -10,40 +10,47 @@ function PostInfo({cards}) {
     console.log('POSTINFO>>>',post)
     return (
         <>
-            <Container  maxWidth="lg" sx={{
+            <Container  fixed sx={{
                 display: "flex",
-                minheight: '600px',
+                minHeight: '600px',
                 backgroundColor: 'white',
                 borderRadius: '20px',
                 padding: '24px',
             }}>
-                <Box sx={{maxWidth: '576px'}}>
 
-                    <CardMedia
-                        component="img"
-                        alt="green iguana"
-                        // height="250"
-                        image={post.image}
-                        sx={{borderRadius: '15px'}}
-                    />
-                    <Typography variant={'h4'}>{post.title}</Typography>
-                    <Typography>{post.text}</Typography>
+                <Grid container spacing={4}>
+                    <Grid item xs={12}>
+                        <CardMedia
+                            component="img"
+                            alt="foto"
+                            image={post.image}
+                            sx={{borderRadius: '15px'}}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant={'h4'}>{post.title}</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography>{post.text}</Typography>
+                    </Grid>
+                </Grid>
 
-                </Box>
-                <Box sx={{maxWidth: '576px'}}>
+                <Grid container spacing={4}>
+                    <Box sx={{maxWidth: '576px'}}>
 
-                    <Box sx={{display: "flex", alignItems: 'center', justifyContent: 'space-around', width: '576px'}}>
-                        <Box>
-                            <Typography>Автор: {post.author.name}</Typography>
-                            <br/>
-                            <Typography>Прфессия: {post.author.about}</Typography>
+                        <Box sx={{display: "flex", alignItems: 'center', justifyContent: 'space-around', width: '576px'}}>
+                            <Box>
+                                <Typography>Автор: {post.author.name}</Typography>
+                                <br/>
+                                <Typography>Прфессия: {post.author.about}</Typography>
+                            </Box>
+                            <Avatar src={post.author.avatar} sx={{width: 56, height: 56}} alt="Remy Sharp"/>
+
+
                         </Box>
-                        <Avatar src={post.author.avatar} sx={{width: 56, height: 56}} alt="Remy Sharp"/>
-
 
                     </Box>
-
-                </Box>
+                </Grid>
 
 
             </Container>
